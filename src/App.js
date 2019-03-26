d40b44937368a68eba0ad4a502d76775ffecf2eb
lrg1980 <admin@binweb.net>
2019-03-26 11:02:13 -0300

    Inicio de proyecto

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from './componentes/Header';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header
+          titulo = 'Clima WebWeather'/>
+      </div>
+    );
+  }
+}
+
+export default App;
