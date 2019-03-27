f26a48eabc709630dec3c0b76822e404a4a81233
lrg1980 <admin@binweb.net>
2019-03-27 18:27:47 -0300

    Cambios en Clima.js

--- src/App.js
+++ src/App.js
@@ -12,8 +12,10 @@ class App extends Component {
     resultado: {}
   }
 
-  componentDidUpdate() {
-    this.consultarApi();
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.consulta !== this.state.consulta) {
+      this.consultarApi();
+    }
   }
   componentDidMount() {
     this.setState({
--- src/componentes/Clima.js
+++ src/componentes/Clima.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react'
 
 class Clima extends Component {
+
+     mostrarResultado = () => {
+
+          // obtener los datos de la consulta
+          const { name, weather, main } = this.props.resultado;
+          // Si no hay nombre, info de clima y info de max y min de temp, devuelve nulo.
+          if (!name || !weather || !main) return null;
+
+          const kelvin = 273.15;
+
+          return (
+               <div className="row">
+                    <div className="resultado col s12 m8 l6 offset-m3 offset-l3">
+                         <div className="card-panel light-blue align-center">
+                              <span className="white-text align-center">
+                                   <h2>Resultado del clima: <b>{name}</b></h2>
+                                   <p className="temperatura">
+                                        Actual: { (main.temp - kelvin).toFixed(1)} &deg;C
+                                   </p>
+                              </span>                         
+                         </div>
+
+                    </div>
+               </div>
+               
+          )
+     }
      
      render() { 
+          console.log(this.props.resultado);
           return (
                <div className="container">
-                    <h1>Desde Clima.js</h1>
+                    {this.mostrarResultado()}
                </div>
             );
      }
